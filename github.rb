e90b01452ddb0177400c87e5f07d333a3e8af9ff
KristaCalleja <kristacm@gmail.com>
2021-04-22 13:00:07 -0300

    Rest API through github repos

--- github.rb
+++ github.rb
@@ -1,1 +1,18 @@
 require 'rest-client'
+require 'json'
+
+url = "https://api.github.com/search/repositories?"
+query = "lewagon"
+language = "ruby"
+
+response = RestClient.get "#{url}q=#{query}&language=#{language}"
+
+# returns the status
+puts response.code
+# returns a JSON with content
+puts response.body
+# parse the JSON and turn it to a hash
+puts JSON.parse(response.body)
+# extract info from hash, using this as a guide: https://docs.github.com/en/rest/reference/search
+puts JSON.parse(response.body)["total_count"]
+puts JSON.parse(response.body)["items"][18]["description"]
